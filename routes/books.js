920ce59d9e25d88bbf6b25f4d8bf7dd690eceaea
“Sanuzia <sanuziajorge@gmail.com>
2025-11-13 22:01:45 +0000

    Last changes

--- routes/books.js
+++ routes/books.js
@@ -1,29 +1,74 @@
-// Create a new router
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
-});
-
-router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
-});
-
+// Route: List all books
 router.get('/list', function(req, res, next) {
-    let sqlquery = "SELECT * FROM books"; // query database to get all the books
-    // execute sql query
+    let sqlquery = "SELECT * FROM books";
     db.query(sqlquery, (err, result) => {
         if (err) {
             console.error(err);
-            next(err); // pass error to express error handler
+            next(err);
         } else {
-            res.send(result); // send the books data as JSON
+            res.render("list.ejs", { availableBooks: result });
         }
     });
 });
 
+// Route: Add Book form
+router.get('/addbook', function(req, res, next) {
+    res.render("addbook.ejs");
+});
 
-// Export the router object so index.js can access it
-module.exports = router
+// Route: Handle Add Book form submission
+router.post('/bookadded', function (req, res, next) {
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+    let newrecord = [req.body.name, req.body.price];
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            console.error(err);
+            next(err);
+        } else {
+            res.send('This book is added to the database. Name: '+ req.body.name + ', Price: £'+ req.body.price);
+        }
+    });
+});
+
+// Route: Bargain books (price < £20)
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT name, price FROM books WHERE price < 20";
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.render("list.ejs", { availableBooks: result });
+        }
+    });
+});
+
+// Route: Search page (form)
+router.get('/search', function(req, res, next){
+    res.render("search.ejs"); // this page contains the input form
+});
+
+// Route: Handle search query (supports advanced partial match)
+router.get('/search-result', function(req, res, next) {
+    let keyword = req.query.keyword;
+    if (!keyword) {
+        // If empty, just return no results or redirect back
+        return res.render("list.ejs", { availableBooks: [] });
+    }
+    
+    // Advanced search using LIKE (partial match)
+    let sqlquery = "SELECT name, price FROM books WHERE name LIKE ?";
+    let searchTerm = '%' + keyword + '%';
+
+    db.query(sqlquery, [searchTerm], (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.render("list.ejs", { availableBooks: result });
+        }
+    });
+});
+
+module.exports = router;

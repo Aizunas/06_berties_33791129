b7c6b974fa8d4df75182a589c708220937f3b8b7
“Sanuzia <sanuziajorge@gmail.com>
2025-11-13 20:04:16 +0000

    Added MySQL integration and app user setup

--- routes/books.js
+++ routes/books.js
@@ -11,5 +11,19 @@ router.get('/search-result', function (req, res, next) {
     res.send("You searched for: " + req.query.keyword)
 });
 
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            console.error(err);
+            next(err); // pass error to express error handler
+        } else {
+            res.send(result); // send the books data as JSON
+        }
+    });
+});
+
+
 // Export the router object so index.js can access it
 module.exports = router
